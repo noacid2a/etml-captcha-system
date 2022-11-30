1c1b4ccd4e74845d48394364f787ea11f0c501dc
Noa Henry Chouriberry <pz31luo@eduvaud.ch>
2022-11-30 10:14:58 +0100

    added captcha input in form

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 
 <form name="form" method="post" action="mailto:yourmail@mail.com">
     <div>
-        <label for="subject">Objet</label><br/>
+        <label for="subject">Objet</label><br />
         <input type="text" placeholder="Subject" name="subject" required />
     </div><br/>
     <div>
-        <label for="lname">Message</label><br/>
+        <label for="lname">Message</label><br />
         <textarea placeholder="Message" name="message" required></textarea>
     </div><br/>
     <div>
-    <label>Entrez le captcha: </label>
-    <!-- img captcha ici-->
-    <input type="text" name="captcha" required/>
-    </div>
+    <label>Entrez le captcha: </label><br />
+    <p><img src="captcha.php?rnd=<?= rand(); ?>" id='captchaImage'></p>
+
+    <label for="captcha">Rafraichir le captcha - <a href="">cliquez ici</a></label><br />
+        <input type="text" name="captcha" required/>
+    </div><br />
     <div>
         <button type="submit">Envoyer</button>
     </div>
