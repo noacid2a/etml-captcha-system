3dc47918cd41e9fa3e8b2571607e753b96d46449
noacid2a <noacid2a@gmail.com>
2022-11-30 11:18:47 +0100

    updated form and added captcha

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
+<html>
+    <head>
+        <title>Captcha Form</title>
+    </head>
+<body>
+<h1>Test Captcha</h1>
+    <form name="form" method="post" action="">
+        <div>
+            <label for="subject">Objet</label><br />
+            <input type="text" placeholder="Subject" name="subject" required />
+        </div><br/>
+        <div>
+            <label for="lname">Message</label><br />
+            <textarea placeholder="Message" name="message" required></textarea>
+        </div><br/>
+        <div>
+        <label>Entrez le captcha: </label><br />
+        <p><img src="genCaptcha.php?newCaptcha=<?= rand(); ?>" id="captchaImg" ></p>
 
-<form name="form" method="post" action="mailto:yourmail@mail.com">
-    <div>
-        <label for="subject">Objet</label><br />
-        <input type="text" placeholder="Subject" name="subject" required />
-    </div><br/>
-    <div>
-        <label for="lname">Message</label><br />
-        <textarea placeholder="Message" name="message" required></textarea>
-    </div><br/>
-    <div>
-    <label>Entrez le captcha: </label><br />
-    <p><img src="captcha.php?rnd=<?= rand(); ?>" id='captchaImage'></p>
-
-    <label for="captcha">Rafraichir le captcha - <a href="">cliquez ici</a></label><br />
-        <input type="text" name="captcha" required/>
-    </div><br />
-    <div>
-        <button type="submit">Envoyer</button>
-    </div>
-</form>
+        <label for="captcha">Rafraichir le captcha - <a href="">cliquez ici</a></label><br />
+            <input type="text" name="captcha" required/>
+        </div><br />
+        <div>
+        <input type="submit" name="submit" value="Submit">
+        </div>
+    </form>
+</body>
+</html>
